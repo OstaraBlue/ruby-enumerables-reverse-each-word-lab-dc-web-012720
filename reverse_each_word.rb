4df42e2ba9e2f2463126891fa1ff39fb74cc7d81
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 18:27:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
 result = []
 split_sentence = string.split
 split_sentence.each {|word| word.reverse}
-result << split_sentence.join
-result
+result << split_sentence
+result.join(" ")
 end
 
 
