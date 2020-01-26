cf6bbcb0cb75bd5aa642944084cd5af6d06f161c
Sonata Arrington <sarrington1009@gmail.com>
2020-01-26 17:56:30 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  result = []
+  original_array = string.split
+  original_array.each do |string|
+    result << string.reverse
+  end
+  result.join
+end
+
+
+def reverse_each_word(string)
+  result = []
+  original_array = string
+end 
